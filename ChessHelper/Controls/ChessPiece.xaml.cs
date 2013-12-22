37c0d073f561b562ddf83705ec713f2ef1814192
Dave Vronay <davevr@eweware.com>
2013-12-21 18:23:36 -0800

    Initial Checkin

---
+++ ChessHelper/Controls/ChessPiece.xaml.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessHelper
+{
+    public class PieceLoc
+    {
+        public PieceLoc(int theRow, int theCol)
+        {
+            Row = theRow;
+            Column = theCol;
+        }
+
+        public int Row {get; set;}
+        public int Column {get; set;}
+
+        public bool IsBlack
+        {
+            get
+            {
+                if (Row % 2 == 0)
+                {
+                    if (Column % 2 == 0)
+                        return false;
+                    else
+                        return true;
+                }
+                else
+                {
+                    if (Column % 2 == 0)
+                        return true;
+                    else
+                        return false;
+                }
+            }  
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for ChessPiece.xaml
+    /// </summary>
+    public partial class ChessPiece : UserControl
+    {
+        private int _row = 0;
+        private int _col = 0;
+        private bool _isBlack = false;
+        private string pieceType = "bishop";
+        private bool _selected = false;
+        private static Brush HighlightBrush = new SolidColorBrush(Colors.Yellow);
+        private static Brush TransparentBrush = new SolidColorBrush(Colors.Transparent);
+
+        public ChessPiece()
+        {
+            InitializeComponent();
+        }
+
+        public bool Selected
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+                if (_selected)
+                    Background = HighlightBrush;
+                else
+                    Background = TransparentBrush;
+            }
+        }
+
+        public static ChessPiece CreatePiece(string pieceName, bool isBlack)
+        {
+            ChessPiece newPiece = new ChessPiece();
+            string imageStr = "Chess_";
+
+            newPiece._isBlack = isBlack;
+            newPiece.pieceType = pieceName;
+            
+            switch (pieceName)
+            {
+                case "rook":
+                    imageStr += "r";
+                    break;
+                case "knight":
+                    imageStr += "n";
+                    break;
+                case "bishop":
+                    imageStr += "b";
+                    break;
+                case "king":
+                    imageStr += "k";
+                    break;
+                case "queen":
+                    imageStr += "q";
+                    break;
+                case "pawn":
+                    imageStr += "p";
+                    break;
+            }
+
+            if (isBlack)
+                imageStr += "d";
+            else
+                imageStr += "l";
+            imageStr += "t60.png";
+
+            newPiece.PieceImage.Source = new BitmapImage(new Uri("../Assets/" + imageStr, UriKind.Relative));
+
+            return newPiece;
+        }
+
+        public void SetPieceLocation(int row, int col)
+        {
+            _row = row;
+            _col = col;
+            UpdatePieceLoc();
+        }
+
+        public int Row
+        {
+            get { return _row; }
+            set
+            {
+                _row = value;
+                UpdatePieceLoc();
+            }
+        }
+
+        public bool IsBlack
+        {
+            get { return _isBlack; }
+            
+        }
+
+        public int Column
+        {
+            get { return _col; }
+            set
+            {
+                _col = value;
+                UpdatePieceLoc();
+            }
+        }
+
+        private void UpdatePieceLoc()
+        {
+            Grid.SetRow(this, Row);
+            Grid.SetColumn(this, Column);
+            if (this.Parent != null)
+            {
+                
+            }
+        }
+
+        public List<PieceLoc> GetPossibleMoves()
+        {
+            List<PieceLoc> possibleMoves = null;
+            switch (pieceType)
+            {
+                case "rook":
+                    possibleMoves = GetRookMoves();
+                    break;
+                case "knight":
+                    possibleMoves = GetKnightMoves();
+                    break;
+                case "bishop":
+                    possibleMoves = GetBishopMoves();
+                    break;
+                case "king":
+                    possibleMoves = GetKingMoves();
+                    break;
+                case "queen":
+                    possibleMoves = GetQueenMoves();
+                    break;
+                case "pawn":
+                    possibleMoves = GetPawnMoves();
+                    break;
+            }
+
+
+            return possibleMoves;
+        }
+
+        private List<PieceLoc> GetPawnMoves()
+        {
+            List<PieceLoc> possibleMoves = new List<PieceLoc>();
+            
+            if (IsBlack)
+            {
+                possibleMoves.Add(new PieceLoc(Row + 1, Column - 1));
+                possibleMoves.Add(new PieceLoc(Row + 1, Column + 1));
+            }
+            else
+            {
+                possibleMoves.Add(new PieceLoc(Row - 1, Column - 1));
+                possibleMoves.Add(new PieceLoc(Row - 1, Column + 1));
+            }
+            
+
+            return TrimMoveList(possibleMoves);
+        }
+
+        private List<PieceLoc> GetBishopMoves()
+        {
+            List<PieceLoc> possibleMoves = new List<PieceLoc>();
+            int curRow = Row, curCol = Column;
+            ChessPiece curPiece;
+            PieceLoc curLoc;
+
+            for (int i = 1; i < 8; i++ )
+            {
+                curRow++;
+                curCol++;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                 possibleMoves.Add(curLoc);
+            }
+
+            curRow = Row; curCol = Column;
+            for (int i = 1; i < 8; i++)
+            {
+                curRow--;
+                curCol++;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+            curRow = Row; curCol = Column;
+            for (int i = 1; i < 8; i++)
+            {
+                curRow++;
+                curCol--;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+            curRow = Row; curCol = Column;
+            for (int i = 1; i < 8; i++)
+            {
+                curRow--;
+                curCol--;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+
+
+            return TrimMoveList(possibleMoves);
+        }
+
+        private List<PieceLoc> GetRookMoves()
+        {
+            List<PieceLoc> possibleMoves = new List<PieceLoc>();
+            int curRow = Row, curCol = Column;
+            ChessPiece curPiece;
+            PieceLoc curLoc;
+
+            for (int i = 1; i < 8; i++)
+            {
+                curRow++;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+            curRow = Row; curCol = Column;
+            for (int i = 1; i < 8; i++)
+            {
+                curRow--;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+            curRow = Row; curCol = Column;
+            for (int i = 1; i < 8; i++)
+            {
+                curCol++;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+            curRow = Row; curCol = Column;
+            for (int i = 1; i < 8; i++)
+            {
+                curCol--;
+                curLoc = new PieceLoc(curRow, curCol);
+                curPiece = PieceAtLocation(curLoc);
+                if (curPiece != null)
+                {
+                    if (curPiece.IsBlack != IsBlack)
+                        possibleMoves.Add(curLoc);
+                    break;
+                }
+                else
+                    possibleMoves.Add(curLoc);
+            }
+
+            return TrimMoveList(possibleMoves);
+        }
+
+        private ChessPiece PieceAtLocation(PieceLoc theLoc)
+        {
+            return ((MainWindow)Window.GetWindow(this)).PieceAtLocation(theLoc);
+        }
+
+        private List<PieceLoc> TrimMoveList(List<PieceLoc> theList)
+        {
+            List<PieceLoc> possibleMoves = new List<PieceLoc>();
+
+            foreach (PieceLoc theLoc in theList)
+            {
+                if ((theLoc.Row >= 0) &&
+                    (theLoc.Row < 8) &&
+                    (theLoc.Column >= 0) &&
+                    (theLoc.Column < 8))
+                {
+                    ChessPiece curPiece = PieceAtLocation(theLoc);
+                    if ((curPiece == null) || (curPiece.IsBlack != this.IsBlack))
+                    {
+                        possibleMoves.Add(theLoc);
+                    }
+                   
+                }
+            }
+
+            return possibleMoves;
+        }
+
+
+        private List<PieceLoc> GetKnightMoves()
+        {
+            List<PieceLoc> possibleMoves = new List<PieceLoc>();
+            possibleMoves.Add(new PieceLoc(Row - 1, Column - 2));
+            possibleMoves.Add(new PieceLoc(Row - 1, Column + 2));
+            possibleMoves.Add(new PieceLoc(Row + 1, Column - 2));
+            possibleMoves.Add(new PieceLoc(Row + 1, Column + 2));
+            possibleMoves.Add(new PieceLoc(Row - 2, Column - 1));
+            possibleMoves.Add(new PieceLoc(Row - 2, Column + 1));
+            possibleMoves.Add(new PieceLoc(Row + 2, Column - 1));
+            possibleMoves.Add(new PieceLoc(Row + 2, Column + 1));
+
+            return TrimMoveList(possibleMoves);
+        }
+
+        private List<PieceLoc> GetKingMoves()
+        {
+            List<PieceLoc> possibleMoves = new List<PieceLoc>();
+            possibleMoves.Add(new PieceLoc(Row - 1, Column - 1));
+            possibleMoves.Add(new PieceLoc(Row - 1, Column + 1));
+            possibleMoves.Add(new PieceLoc(Row + 1, Column - 1));
+            possibleMoves.Add(new PieceLoc(Row + 1, Column + 1));
+            possibleMoves.Add(new PieceLoc(Row - 1, Column));
+            possibleMoves.Add(new PieceLoc(Row + 1, Column));
+            possibleMoves.Add(new PieceLoc(Row, Column - 1));
+            possibleMoves.Add(new PieceLoc(Row, Column + 1));
+
+            return TrimMoveList(possibleMoves);
+        }
+
+        private List<PieceLoc> GetQueenMoves()
+        {
+            List<PieceLoc> rookMoves = GetRookMoves();
+            List<PieceLoc> bishopMoves = GetBishopMoves();
+            List<PieceLoc> possibleMoves = rookMoves.Union(bishopMoves).ToList();
+
+
+            return possibleMoves;
+        }
+
+    }
+}
---
+++ ChessHelper/MainWindow.xaml.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessHelper
+{
+
+    public class ThreatCounter
+    {
+        static private Brush TransparentBrush = new SolidColorBrush(Colors.Transparent);
+        private int _whiteCount = 0;
+        private int _blackCount = 0;
+
+        public Rectangle Marker { get; set; }
+
+        public ThreatCounter(Rectangle theRect)
+        {
+            Marker = theRect;
+            WhiteCount = 0;
+            BlackCount = 0;
+            RecomputeColor();
+        }
+
+        public void Clear()
+        {
+            WhiteCount = 0;
+            BlackCount = 0;
+            RecomputeColor();
+        }
+
+        public int WhiteCount 
+        { 
+            get {return _whiteCount;}
+            set
+            {
+                _whiteCount = value;
+                RecomputeColor();
+            }
+        }
+        public int BlackCount
+        {
+            get {return _blackCount;}
+            set
+            {
+                _blackCount = value;
+                RecomputeColor();
+            }
+        }
+
+        public void RecomputeColor()
+        {
+            if ((_whiteCount == 0) && (_blackCount == 0))
+            {
+                Marker.Fill = TransparentBrush;
+            }
+            else
+            {
+                int redPart = 64 * BlackCount;
+                if (redPart > 0)
+                    redPart = 255;
+                int greenPart = 0;
+
+                int bluePart = 64 * WhiteCount;
+                if (bluePart > 0)
+                    bluePart = 255;
+                
+                if ((redPart > 0) && (bluePart > 0))
+                {
+                    redPart = 128;
+                    greenPart = 64;
+                    redPart = 128;
+                }
+
+
+                Marker.Fill = new SolidColorBrush(Color.FromArgb(255, (byte)redPart, (byte)greenPart, (byte)bluePart));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ThreatCounter[,] heatMap = new ThreatCounter[8, 8];
+        private ChessPiece _curPiece = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BuildBoard();
+        }
+
+        private void BuildBoard()
+        {
+            Brush BlackBrush = new SolidColorBrush(Color.FromArgb(255,128,128,128));
+            Brush WhiteBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+            Brush TransparentBrush = new SolidColorBrush(Colors.Transparent);
+            bool drawBlack = false;
+            Rectangle newRect, heatRect;
+
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    
+                     newRect = new Rectangle();
+                    newRect.Stroke = BlackBrush;
+                    newRect.SnapsToDevicePixels = true;
+                    if (drawBlack)
+                        newRect.Fill = BlackBrush;
+                    else
+                        newRect.Fill = WhiteBrush;
+                    drawBlack = !drawBlack;
+                    Grid.SetRow(newRect, row);
+                    Grid.SetColumn(newRect, col);
+                    ChessBoard.Children.Add(newRect);
+                    
+
+                    heatRect = new Rectangle();
+                    heatRect.Stroke = BlackBrush;
+                    heatRect.SnapsToDevicePixels = true;
+                    heatRect.Fill = new SolidColorBrush(Colors.Blue);
+                    heatRect.IsHitTestVisible = false;
+                    Grid.SetRow(heatRect, row);
+                    Grid.SetColumn(heatRect, col);
+                    ChessHeatMap.Children.Add(heatRect);
+                    heatMap[row, col] = new ThreatCounter(heatRect);
+
+                }
+                drawBlack = !drawBlack;
+            }
+        }
+
+        public ChessPiece PieceAtLocation(PieceLoc theLoc)
+        {
+            foreach (ChessPiece curPiece in ChessPieceLayer.Children)
+            {
+                if ((curPiece.Row == theLoc.Row) &&
+                    (curPiece.Column == theLoc.Column))
+                    return curPiece;
+            }
+
+            return null;
+        }
+
+        public ChessPiece SelectedPiece
+        {
+            get { return _curPiece; }
+            set
+            {
+                if (_curPiece != null)
+                    _curPiece.Selected = false;
+                _curPiece = value;
+                if (_curPiece != null)
+                    _curPiece.Selected = true;
+            }
+        }
+
+        private void ResetBoard()
+        {
+            CreateAndAddPiece("rook", 0, 0, true);
+            CreateAndAddPiece("knight", 0, 1, true);
+            CreateAndAddPiece("bishop", 0, 2, true);
+            CreateAndAddPiece("queen", 0, 3, true);
+            CreateAndAddPiece("king", 0, 4, true);
+            CreateAndAddPiece("bishop", 0, 5, true);
+            CreateAndAddPiece("knight", 0, 6, true);
+            CreateAndAddPiece("rook", 0, 7, true); 
+
+            CreateAndAddPiece("pawn", 1, 0, true);
+            CreateAndAddPiece("pawn", 1, 1, true);
+            CreateAndAddPiece("pawn", 1, 2, true);
+            CreateAndAddPiece("pawn", 1, 3, true);
+            CreateAndAddPiece("pawn", 1, 4, true);
+            CreateAndAddPiece("pawn", 1, 5, true);
+            CreateAndAddPiece("pawn", 1, 6, true);
+            CreateAndAddPiece("pawn", 1, 7, true);
+
+
+            CreateAndAddPiece("pawn", 6, 0, false);
+            CreateAndAddPiece("pawn", 6, 1, false);
+            CreateAndAddPiece("pawn", 6, 2, false);
+            CreateAndAddPiece("pawn", 6, 3, false);
+            CreateAndAddPiece("pawn", 6, 4, false);
+            CreateAndAddPiece("pawn", 6, 5, false);
+            CreateAndAddPiece("pawn", 6, 6, false);
+            CreateAndAddPiece("pawn", 6, 7, false);
+
+
+            CreateAndAddPiece("rook", 7, 0, false);
+            CreateAndAddPiece("knight", 7, 1, false);
+            CreateAndAddPiece("bishop", 7, 2, false);
+            CreateAndAddPiece("queen", 7, 3, false);
+            CreateAndAddPiece("king", 7, 4, false);
+            CreateAndAddPiece("bishop", 7, 5, false);
+            CreateAndAddPiece("knight", 7, 6, false);
+            CreateAndAddPiece("rook", 7, 7, false);
+
+
+
+            UpdateHeatMap();
+        }
+
+        private void CreateAndAddPiece(string pieceType, int row, int col, bool isBlack)
+        {
+            ChessPiece newPiece = ChessPiece.CreatePiece(pieceType, isBlack);
+            newPiece.MouseDown += ChessBoard_MouseDown;
+            newPiece.SetPieceLocation(row, col);
+            ChessPieceLayer.Children.Add(newPiece);
+        }
+
+        private void ClearBoard()
+        {
+            ChessPieceLayer.Children.Clear();
+            ClearHeatMap();
+        }
+
+        private void ClearHeatMap()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    heatMap[row, col].Clear();
+                }
+            }
+        }
+
+
+
+        private void UpdateHeatMap()
+        {
+            ClearHeatMap();
+
+            foreach (ChessPiece curPiece in ChessPieceLayer.Children)
+            {
+                List<PieceLoc> LocList = curPiece.GetPossibleMoves();
+                foreach (PieceLoc curLoc in LocList)
+                {
+                    if (curPiece.IsBlack)
+                        heatMap[curLoc.Row, curLoc.Column].BlackCount++;
+                    else
+                        heatMap[curLoc.Row, curLoc.Column].WhiteCount++;
+                    
+                }
+            }
+
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            ResetBoard();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearBoard();
+        }
+
+        private void ChessBoard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is ChessPiece)
+            {
+                if (SelectedPiece != e.OriginalSource)
+                    SelectedPiece = (ChessPiece)e.OriginalSource;
+                else
+                    SelectedPiece = null;
+            }
+            else if (e.OriginalSource is Image)
+            {
+                Grid parent = (Grid)((Image)e.OriginalSource).Parent;
+                if (SelectedPiece != (ChessPiece)parent.Parent)
+                    SelectedPiece = (ChessPiece)parent.Parent;
+                else
+                    SelectedPiece = null;
+            }
+            else if ((e.OriginalSource is Rectangle) && (SelectedPiece != null))
+            {
+                Rectangle theSquare = (Rectangle)e.OriginalSource;
+                int row = Grid.GetRow(theSquare);
+                int col = Grid.GetColumn(theSquare);
+                SelectedPiece.SetPieceLocation(row, col);
+                UpdateHeatMap();
+            }
+            else
+                SelectedPiece = null;
+
+        }
+    }
+}
